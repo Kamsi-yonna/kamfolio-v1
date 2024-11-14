b3be584fd7aec3c4460c22eb9172a2d0c3cca4d3
Kamsi-yonna <etiobikamsi@gmail.com>
2024-11-14 03:44:19 +0100

    feat - setup the projects and articles page

---
+++ pages/projects.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="min-h-screen">
+        <div class="mb-6">
+            <h1 class="text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">Projects</h1>
+            <p class="mt-6 text-base text-gray-600 dark:text-gray-400">
+                {{ description }}
+            </p>
+
+            <div v-for="(project, id) in displayedProjects" :key="id">
+                <NuxtLink class="flex items-end gap-4 group p-2 -m-2 mt-8 rounded-lg" external target="_blank"
+                    :to="project.url">
+                    <div class="max-w-sm">
+                        <h3 class="text-sm">{{ project.name }}</h3>
+                        <p class="text-gray-400 text-sm">{{ project.description }}</p>
+                    </div>
+
+                    <div
+                        class="flex-1 border-b border-dashed border-gray-300 dark:border-gray-800 group-hover:border-gray-700">
+                    </div>
+                    <UAvatar :alt="project.name" size="md" :src="project.thumbnail"
+                        :ui="{ rounded: 'rounded z-10 relative' }" />
+                </NuxtLink>
+            </div>
+
+            <div class="search-box mt-4">
+                <div class="w-full md:w-3/5 mx-auto flex items-center gap-2 p-8">
+                    <UAlert :avatar="{
+                        src: 'https://media.giphy.com/media/NXOF5rlaSXdAc/giphy.gif',
+                        size: '2xl',
+                        base: 'flex-shrink-0 self-center'
+                    }" :description="noArticlesFound" title="Oops! still in prog.." variant="solid" />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+const description =
+    'Discover my projects – a showcase of realistic and accomplished endeavors. Explore a curated collection of works that reflect my expertise and pride in various tech projects developed over the years.'
+
+useSeoMeta({
+    title: 'Projects | Kamsiyonna',
+    description
+})
+</script>
