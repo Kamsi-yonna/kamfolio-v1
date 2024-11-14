f8adb1ad96c0288128d0817e9456a8e146f26dbe
Kamsi-yonna <etiobikamsi@gmail.com>
2024-11-14 16:51:30 +0100

    feat - setup projects page and added site images

--- pages/projects.vue
+++ pages/projects.vue
@@ -6,30 +6,44 @@
                 {{ description }}
             </p>
 
-            <div v-for="(project, id) in displayedProjects" :key="id">
-                <NuxtLink class="flex items-end gap-4 group p-2 -m-2 mt-8 rounded-lg" external target="_blank"
-                    :to="project.url">
-                    <div class="max-w-sm">
-                        <h3 class="text-sm">{{ project.name }}</h3>
-                        <p class="text-gray-400 text-sm">{{ project.description }}</p>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-14 p-8">
+                <div v-for="(project, index) in projects" :key="index" class="project-card">
+                    <NuxtLink :to="project.liveSite" external target="_blank">
+                        <UAvatar :alt="project.name" size="md" :src="project.imageUrl"
+                            class="absolute inset-0 w-full h-full object-cover transition-opacity duration-300 ease-in-out"
+                            :ui="{
+                                rounded: 'rounded-none',
+                                background: 'bg-gray-100 dark:bg-gray-800',
+                                size: {
+                                    md: 'h-full w-full',
+                                    lg: 'h-full w-full',
+                                },
+                                placeholder: 'font-bold text-[80px] leading-none text-gray-500 dark:text-grey-100 truncate'
+                            }" />
+                        <div
+                            class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40 transition-opacity duration-300 ease-in-out">
+                        </div>
+                    </NuxtLink>
+                    <h3 class="z-10 mt-3 text-2xl font-bold text-white transition-colors duration-300 ease-in-out">
+                        {{ project.name }}</h3>
+                    <div class="z-10 gap-y-1 overflow-hidden text-[12px] leading-6 text-gray-300">
+                        <span v-for="(category, catIndex) in project.categories" :key="catIndex">
+                            {{ category }}<span v-if="catIndex < project.categories.length - 1"> &#x2022; </span>
+                        </span>
                     </div>
-
-                    <div
-                        class="flex-1 border-b border-dashed border-gray-300 dark:border-gray-800 group-hover:border-gray-700">
-                    </div>
-                    <UAvatar :alt="project.name" size="md" :src="project.thumbnail"
-                        :ui="{ rounded: 'rounded z-10 relative' }" />
-                </NuxtLink>
+                </div>
             </div>
 
-            <div class="search-box mt-4">
-                <div class="w-full md:w-3/5 mx-auto flex items-center gap-2 p-8">
-                    <UAlert :avatar="{
-                        src: 'https://media.giphy.com/media/NXOF5rlaSXdAc/giphy.gif',
-                        size: '2xl',
-                        base: 'flex-shrink-0 self-center'
-                    }" :description="noArticlesFound" title="Oops! still in prog.." variant="solid" />
-                </div>
+
+            <div
+                class="flex justify-center items-center my-12 space-y-1 sm:flex grid grid-cols-1 gap-3 text-gray-900 dark:text-gray-400">
+                <NuxtLink class="flex items-end gap-8 p-4 border border-gray-800 rounded-xl hover:border-primary"
+                    to="/lab">
+                    <span class="text-sm">
+                        That's all for now, head over to my lab
+                    </span>
+                    <Icon class="w-6 h-6" name="heroicons:beaker"></Icon>
+                </NuxtLink>
             </div>
         </div>
     </main>
@@ -43,4 +57,35 @@ useSeoMeta({
     title: 'Projects | Kamsiyonna',
     description
 })
+
+const projects = [
+    {
+        imageUrl: '/projects/fyree-mag_preview.png',
+        name: 'Fyree Mag',
+        categories: ['Webflow', 'UI Development'],
+        liveSite: 'https://fyrre-mag.webflow.io/'
+    },
+    {
+        imageUrl: '/projects/hayes-valley_preview.png',
+        name: 'Hayes Valley',
+        categories: ['Webflow', 'UI Development'],
+        liveSite: 'https://hayes-kamsiyonna.webflow.io/'
+    },
+    {
+        imageUrl: '/projects/horizon-academy_preview.png',
+        name: 'Horizon Academy',
+        categories: ['HTML/CSS', 'UI Development'],
+        liveSite: 'https://kamsi-yonna.github.io/Horizon-Academy/'
+    },
+    {
+        imageUrl: '/projects/carouselling_preview.png',
+        name: 'Carouselling',
+        categories: ['webflow', 'UI Development'],
+        liveSite: 'https://kam-caroselling.webflow.io/'
+    },
+];
 </script>
+
+<style scoped>
+/* Additional styles can be added here if necessary */
+</style>
