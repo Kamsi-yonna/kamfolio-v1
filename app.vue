cf6ba4d1cae3de046244048fd44b7343b3e77066
Kamsi-yonna <etiobikamsi@gmail.com>
2024-11-14 01:30:50 +0100

    feat - added tailwind and other dependencies

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-red-500">
     This works
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  pages: true,
+  ssr: true,
+  spaLoadingTemplate: false,
+
+  devServer: {
+    port: 3000,
+  },
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+      htmlAttrs: {
+        lang: "en",
+        class: "h-full",
+      },
+      bodyAttrs: {
+        class: "antialiased bg-gray-50 dark:bg-black min-h-screen ",
+      },
+    },
+  },
+
+  modules: [[
+    "@nuxtjs/google-fonts",
+    {
+      families: {
+        Poppins: {
+          wght: [300, 400, 500, 700, 800],
+        },
+      },
+      display: "swap",
+      download: true,
+    },
+  ], "nuxt-icon", "@nuxt/image", ["@nuxt/ui", {}], "@vueuse/nuxt", "@nuxt/content", "@nuxt/ui"],
+
+  ui: {
+    icons: ["heroicons", "lucide"],
+  },
+
+  css: ["~/assets/css/tailwind/main.css"],
+
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind/index.css",
+    configPath: "tailwind.config",
+    injectPosition: "first",
+    viewer: false,
+  },
+
+  components: ["~/components"],
+  compatibilityDate: "2024-11-14",
+});
