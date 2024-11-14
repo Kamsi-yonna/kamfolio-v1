b01ec804ec0ff64e31141ef74a27b019b9c3329e
Kamsi-yonna <etiobikamsi@gmail.com>
2024-11-14 02:14:40 +0100

    fix - added configurations for light/dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+import type { Config } from "tailwindcss";
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        zoop: "rgba(102, 109, 128, 0.08) 0px 1.2672px 1.2672px 0px, rgba(102, 109, 128, 0.08) 0px 5.06879px 10.1376px 0px",
+        zoopdark:
+          "rgba(10, 10, 10, 0.2) 0px 1.2672px 1.2672px 0px, rgba(10, 10, 10, 0.2) 0px 5.06879px 10.1376px 0px",
+      },
+    },
+    fontFamily: {
+      Poppins: [],
+    },
+    keyframes: {
+      pulse: {
+        "0%, 100%": {
+          opacity: "1",
+        },
+        "50%": {
+          opacity: ".8",
+        },
+      },
+    },
+    animation: {
+      pulse: "pulse 1.4s ease-in-out infinite",
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/typography"), "@tailwindcss/animation"],
+} satisfies Config;
